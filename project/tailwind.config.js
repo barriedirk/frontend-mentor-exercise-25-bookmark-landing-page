1196f84072aff8bb3bb39acc9995424e76ee9509
Barrie Freyre <barriedirk@yahoo.com>
2025-05-25 22:38:39 -0500

    adding styles for blue background

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,15 +1,6 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   safelist: [],
-  theme: {
-    extend: {
-      spacing: {
-        90: "90px",
-      },
-      maxWidth: {
-        1440: "90rem", // ✅ Custom class: max-w-1440
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };

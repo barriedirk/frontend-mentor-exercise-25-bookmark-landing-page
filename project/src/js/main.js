027a08dccde3ffd0719f7ec86346ce5d78afa568
Barrie Freyre <barriedirk@yahoo.com>
2025-05-26 20:41:23 -0500

    add styles for mobile and update svg to manipulate the color via css

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -2,18 +2,25 @@ import { initializeTabs } from "./tabs.js";
 import { initializeForm } from "./form.js";
 
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 (async () => {
   // -- Mobile Menu Toggle Button ------
   const $body = $("body");
   const $btnToggle = $("#headerBtnToggle");
+  const $$menuLinks = $$(".header__nav-link");
+
+  $$menuLinks.forEach(($link) => {
+    $link.addEventListener("click", () => {
+      $body.classList.remove("is-expanded");
+      $btnToggle.setAttribute("aria-expanded", "false");
+    });
+  });
 
   $btnToggle.addEventListener("click", async (evt) => {
     evt.preventDefault();
 
     const isExpanded = $btnToggle.getAttribute("aria-expanded") == "true";
-
-    console.log(isExpanded);
 
     $btnToggle.setAttribute("aria-expanded", !isExpanded ? "true" : "false");
 
